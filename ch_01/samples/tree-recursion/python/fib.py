6a8df2128b0f0f7b3ec62fb452ff28c4302d4093
jumzzz <jummel19@gmail.com>
2024-04-08 14:05:50 +0800

    try golden ratio

--- ch_01/samples/tree-recursion/python/fib.py
+++ ch_01/samples/tree-recursion/python/fib.py
@@ -1,11 +1,12 @@
 import argparse
+import math
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add('num', help='number of fibonacci sequence', type=int)
+    parser.add_argument('num', help='number of fibonacci sequence', type=int)
 
-    args = parser.get_args()
+    args = parser.parse_args()
     return args
 
 def fib(n: int):
@@ -13,13 +14,33 @@ def fib(n: int):
         return 0
     if n == 1:
         return 1
+    
     return fib(n - 1) + fib(n  - 2)
+
+def approx_fib(n: int):
+    golden_ratio = (1 + 5**0.5)/2.0
+    approx = golden_ratio ** n / (5 ** 0.5)
+    
+    floor_approx = math.floor(approx)
+    ceil_approx = math.floor(approx)
+
+    diff_ceil = ceil_approx - approx
+    diff_floor = approx - floor_approx
+
+    if min(diff_ceil, diff_floor) == diff_floor:
+        return floor_approx
+    
+    return ceil_approx
+
 
 def main():
     args = get_args()
     num = args.num
     res = fib(num)
+    approx_res = approx_fib(num)
+    
     print(f'fib({num}) = ', res)
+    print(f'apprx_fib({num}) = ', approx_res)
 
 
 if __name__ == '__main__':
